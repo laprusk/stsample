2d81b7f114aca5d3492fa17d5171dc41da700511
laprusk <149079151+laprusk@users.noreply.github.com>
2024-10-28 01:17:16 +0900

    update

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ file_name = None
 def get_credentials():
     credentials_json = os.getenv("GOOGLE_CREDENTIALS")
     if credentials_json is not None:
+        st.write("Using credentials from GOOGLE_CREDENTIALS environment variable.")
         credentials_json = json.dumps(credentials_json)
         credentials_dict = json.loads(credentials_json)
         return Credentials.from_service_account_info(credentials_dict, scopes=SCOPES)
